6cc974ebd015d578600eb9532186aec82f7490d1
Anders Inde <anders.inde@assaabloy.com>
2024-08-12 19:22:22 +0200

    Move bluetooth logic to hook

--- hooks/useBluetooth.ts
+++ hooks/useBluetooth.ts
@@ -1,22 +1,140 @@
-import { Settings, } from 'react-native';
-import { useState } from "react";
+import { Alert, NativeEventEmitter, NativeModules, } from 'react-native';
+import React, { useState } from "react";
+import BleManager from "react-native-ble-manager";
+import { Peripheral as RNPeripheral } from "react-native-ble-manager/dist/esm/types";
 
-export function useRecordSignal(onReceiveChunk: (chunk: Uint8Array) => void) {
+const SERVICE_UUID = '0000aadb-0000-1000-8000-00805f9b34fb';
+const CHARACTERISTIC_UUID = '0000aadc-0000-1000-8000-00805f9b34fb';
 
-  const [recording, setRecording] = useState<boolean>(false);
+interface Peripheral extends RNPeripheral {
+  connected?: boolean;
+}
 
-  const sampleRate = Settings.get('sampleRate');
-  const bitsPerSample = Settings.get('bitsPerSample');
-  const bufferSize = Settings.get('bufferSize');
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
-  const [chunk, setChunk] = useState<Uint8Array>(new Uint8Array());
-  const isRecordingAllowed = !!sampleRate && !!bitsPerSample && !!bufferSize;
+export function useBluetooth() {
 
-  async function startRecording() {
+  const peripherals = new Map<string, Peripheral>();
+  const [isScanning, setIsScanning] = useState(false);
+  const [connectedDevice, setConnectedDevice] = useState<Peripheral | null>(null);
+  const [discoveredDevices, setDiscoveredDevices] = useState<Peripheral[]>([]);
+  const [data, setData] = useState<string | null>(null);
+
+  React.useEffect(() => {
+    BleManager.start({ showAlert: true })
+
+    let stopDiscoverListener = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      peripheral => {
+        peripherals.set(peripheral.id, peripheral);
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        if (peripheral.name === "mpy-uart") {
+          connectToPeripheral(peripheral);
+        }
+      },
+    );
+
+    let stopConnectListener = BleManagerEmitter
+      .addListener('BleManagerConnectPeripheral', peripheral => console.log('BleManagerConnectPeripheral:', peripheral));
+
+    let stopScanListener = BleManagerEmitter.addListener(
+      'BleManagerStopScan',
+      () => setIsScanning(false),
+    );
+
+    return () => {
+      stopDiscoverListener.remove();
+      stopConnectListener.remove();
+      stopScanListener.remove();
+    };
+  }, []);
+
+  const startScan = () => {
+    if (!isScanning) {
+      BleManager.scan([], 4, true)
+        .then(() => {
+          console.log('Scanning...');
+          setIsScanning(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
+
+  const stopScan = () => {
+    BleManager.stopScan()
+      .then(() => {
+        console.log('Scan stopped');
+        setIsScanning(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
-  async function stopRecording() {
+  const connectToPeripheral = (peripheral: Peripheral) => {
+    console.log('connecting to', peripheral.name, peripheral.id);
+
+    BleManager.connect(peripheral.id)
+      .then(() => {
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevice(peripheral);
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        console.log('BLE device paired successfully');
+      })
+      .catch((e) => {
+        console.log('Failed to connect', e);
+      });
+  };
+
+  const disconnectFromPeripheral = (peripheral: Peripheral) => {
+    setData(null);
+    BleManager.disconnect(peripheral.id)
+      .then(() => {
+        peripheral.connected = false;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevice(null);
+      })
+      .catch(() => {
+        console.error('Failed to disconnect');
+      });
+  };
+
+  const read = () => {
+    stopScan();
+    if (!connectedDevice) {
+      return;
+    }
+    BleManager.connect(connectedDevice.id)
+      .then(() => {
+        console.log('connected');
+        return BleManager.retrieveServices(connectedDevice.id);
+      })
+      .then(() => {
+        console.log('retrieveServices');
+        return BleManager.startNotification(connectedDevice.id, SERVICE_UUID, CHARACTERISTIC_UUID);
+      })
+      .then(() => {
+        return BleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', (data) => {
+          // setData(bytesToString(data.value));
+          console.log('data', data.value, String.fromCharCode(...data.value));
+        })
+      })
+      .catch((error) => Alert.alert('Error: ' + error));
   }
 
-  return { chunk, startRecording, stopRecording, recording, isRecordingAllowed, onReceiveChunk }
+  return {
+    discoveredDevices,
+    connectedDevice,
+    isScanning,
+    startScan,
+    stopScan,
+    connectToPeripheral,
+    disconnectFromPeripheral,
+    read,
+    data
+  };
 }
