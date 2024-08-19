58b1b878d2f8b2a2eb1dd88198d97437d9dd7952
Anders Inde <anders.inde@assaabloy.com>
2024-08-19 22:49:33 +0200

    learn how to calculate the circumference of a circle

--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,3 @@
 export const get_wheel_speed_kmh_from_frequency = (frequency: number, wheelDiameter: number) => {
-  return frequency * (wheelDiameter/2000) * Math.PI * 3.6; // [Hz] * [m] = [m/s]
+  return frequency * (wheelDiameter/1000) * Math.PI * 3.6;
 }
